8133f645b6f972549e5add46478001dc091921fb
Marcos Leiva <estudioleiva@gmail.com>
2026-02-16 00:35:50 -0300

    pipeline auditoria drive+vision+reportes

--- main.py
+++ main.py
@@ -31,6 +31,18 @@ from reportlab.lib.utils import simpleSplit
 # =========================================================
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://bi.bymovi.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # =========================================================
 # CONFIG
